f73b30e273ac4ba2fe65ed54b229e266db4b80b1
Tyler Long <tyler4long@gmail.com>
2012-06-19 23:04:50 -0700

    Now we have all necessary functionalities

--- lib/followable.rb
+++ lib/followable.rb
@@ -6,5 +6,17 @@ module Followable
         self.followships.map(&:user)
       end
     EOF
+    unless User.instance_methods.include?(:follow)
+      User.class_eval <<-EOF
+        def follow(model)
+          self.followships.create(followable: model)
+        end
+      EOF
+    end
+    User.class_eval <<-EOF
+      def followed_#{model_class.to_s.downcase.pluralize}
+        self.followships.map(&:followable).select{ |item| item.class==#{model_class.to_s} }
+      end
+    EOF
   end
 end
